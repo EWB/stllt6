7554b38d28fb48734cf7f84af06ceb01e685e1c7
EWB <ewil@stlawu.edu>
2015-08-16 11:57:08 -0400

    initial commit

---
+++ sites/all/themes/marinelli/block.tpl.php
@@ -0,0 +1,6 @@
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?>">
+<div class="defaultblock">
+    <h2><?php print $block->subject; ?></h2><!--block title-->
+   <div class="blockcontent"><?php print $block->content; ?></div>
+</div>
+</div>
